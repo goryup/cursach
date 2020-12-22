a1f391b281c26f88803418e7c5549b32186bea7e
goryup <71817790+goryup@users.noreply.github.com>
2020-12-22 03:12:44 +0300

    Add files via upload

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -4,7 +4,8 @@ import subprocess
 import winapps
 import difflib as df
 
-def w_f(name):
+#запись списка установленных программ 
+def w_f(name): 
     f = open(name,'w')
     try:
         for apps in winapps.list_installed():
@@ -12,16 +13,16 @@ def w_f(name):
     except:
         print()
     f.close()
-
+#чтение файла, вывод текста
 def r_f(name):
     file = open(name)
     text = file.readlines()
     print(name)
     file.close()
     return text
-    
+#меню    
 def gui():
-    print('MENU')
+    print('- MENU -')
     print('1 - list of installed programs') #список установленных программ
     print('2 - to compare the list of installed programs') #сравнить списки установленных программ
     print()
@@ -29,12 +30,12 @@ def gui():
     print()
     print('Enter the command number:')
     return int(input())
-
+#оформление записи списка программ
 def command_1():
     print('Enter file name:')
     name = input()
     w_f(name)
-
+#оформление сравнения файлов со списками программ
 def command_2():
     print('Enter the name of the first file:')
     f1 = input()
@@ -45,8 +46,8 @@ def command_2():
     if a == False:
         dif(f1,f2)
     else:
-        print('No changes!')
-
+        print('--- No changes!')
+#сравнивание и вывод различий в списках
 def dif(f1,f2):
     txt1_list = r_f(f1)
     txt2_list = r_f(f2)
@@ -55,7 +56,7 @@ def dif(f1,f2):
     diff = df.unified_diff(txt1_list, txt2_list, lineterm='')
     print('\n'.join(diff))
 
-    
+   
 a = gui()
 while a != 0:
     if a == 1:
@@ -64,11 +65,14 @@ while a != 0:
         a = gui()
     else: 
         if a == 2:
-            command_2()
+            try:
+                command_2()
+            except:
+                   print('invalid input') 
             print('---')
             a = gui()
         else: 
-            print('Wrong answer. Repeat, please')
+            print('--- Wrong answer. Repeat, please')
             print('---')
             a = gui()
 
